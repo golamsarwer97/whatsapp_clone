02dc59d4a1fd380421acb27f9aaec2e1fb54c28d
Golam Sawer <golamsarwer97@gmail.com>
2023-08-12 01:20:43 +0600

    configured firebase for android & ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'utils/colors.dart';
+import './firebase_options.dart';
+import './utils/colors.dart';
 import './responsive/responsive_layout.dart';
 import './screens/mobile_screen.dart';
 import './screens/web_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
